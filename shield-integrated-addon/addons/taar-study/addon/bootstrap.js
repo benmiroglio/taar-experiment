eed60d20691731ee4f2bff7eb2cf910745ceb617
benmiroglio <bmiroglio@mozilla.com>
2017-09-27 10:43:54 -0700

    Force Load discover tab

--- shield-integrated-addon/addons/taar-study/addon/bootstrap.js
+++ shield-integrated-addon/addons/taar-study/addon/bootstrap.js
@@ -196,8 +196,9 @@ await studyUtils.startup({reason});
       studyUtils.telemetry(dataOut)
       }
       else if (msg['trigger-popup']) {
+        client.sawPopup = true;
         // set pref to force discovery page
-        Preferences.set("extensions.ui.LastCategory", "addons://discover/")
+        Preferences.set("extensions.ui.lastCategory", "addons://discover/")
         var window = Services.wm.getMostRecentWindow('navigator:browser')
         var pageAction = window.document.getElementById("taarexp_mozilla_com-page-action")
         pageAction.click()
